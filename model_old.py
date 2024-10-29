ec5e38b8d39628525c67512a8c126ff37e1a3627
Tomoqt <francesco.pappone99@gmail.com>
2024-10-29 10:45:09 +0100

    asd

--- model_old.py
+++ model_old.py
@@ -303,7 +303,7 @@ class GPT(nn.Module):
         return mfu
 
     @torch.no_grad()
-    def generate(self, idx, max_new_tokens, temperature=1.0, top_k=None):
+    def generate(self, idx, max_new_tokens, temperature=1.0, top_k=300):
         """
         Take a conditioning sequence of indices idx (LongTensor of shape (b,t)) and complete
         the sequence max_new_tokens times, feeding the predictions back into the model each time.
